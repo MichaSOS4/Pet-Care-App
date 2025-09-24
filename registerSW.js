cecbc7b9c0a2b9091b0373cedba03617aa40033a
michasos4 <michaoo3@wp.pl>
2025-09-24 11:35:43 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Pet-Care-App/sw.js', { scope: '/Pet-Care-App/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
